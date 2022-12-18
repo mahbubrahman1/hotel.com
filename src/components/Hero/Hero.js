d49d72477f3e50142ed85831420863507540d329
mahbubrahman1 <mahbuburrahman3@yahoo.com>
2022-12-18 16:37:37 +0600

    finished hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -3,8 +3,36 @@ import HeroImg from '../../assets/images/hero.jpg'
 import images from '../../assets/images/cover-images'
 import { motion } from 'framer-motion'
 import '../../App.css';
+import { Link } from 'react-router-dom';
 
 const Hero = () => {
+    const places = [
+        {
+            title: "Cox's Bazar",
+            image: 'https://i.ibb.co/68CQFrs/coxs-bazar.jpg',
+        },
+        {
+            title: "Sajek Valley",
+            image: 'https://i.ibb.co/x76XXMc/sajek.jpg',
+        },
+        {
+            title: "Shreemangal",
+            image: 'https://i.ibb.co/gdv6SpB/shreemangal.jpg',
+        },
+        {
+            title: "Jaflong",
+            image: 'https://i.ibb.co/K09jchn/jaflong.jpg',
+        },
+        {
+            title: "St. Martin",
+            image: 'https://i.ibb.co/jvNjHTK/st-martin.jpg',
+        },
+        {
+            title: "Sundarban",
+            image: 'https://i.ibb.co/Z2xfY5M/sundarban.jpg',
+        },
+    ]
+
     const [width, setWidth] = useState(0)
     const carousel = useRef();
 
@@ -18,10 +46,12 @@ const Hero = () => {
             <motion.div ref={carousel} className='carousel' whileTap={{ cursor: "grabbing" }}>
                 <motion.div drag='x' dragConstraints={{ right: 0, left: -width }} className='inner-carousel'>
                     {
-                        images.map(image => {
+                        places.map(place => {
                             return (
-                                <motion.div className='item' key={image}>
-                                    <img src={image} alt="" />
+                                <motion.div className='item' key={place.title}>
+                                    <img src={place.image} alt="" className='place-image' />
+                                    <h2 className='card-title'>{place.title}</h2>
+                                    <Link className='card-text'>Book Now</Link>
                                 </motion.div>
                             )
                         })
