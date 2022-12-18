468c60e9d30b4ccab34dc8b558f1edafc1f25220
mahbubrahman1 <mahbuburrahman3@yahoo.com>
2022-12-18 09:25:56 +0600

    added hero section

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import HeroImg from '../../assets/images/hero.jpg'
+
+const Hero = () => {
+    return (
+        <div >
+            <img src={HeroImg} alt="" className='hero' />
+        </div>
+    )
+}
+
+export default Hero
