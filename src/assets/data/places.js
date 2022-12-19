6f00144640d32487c1b2d24eb56798d87d0e553e
mahbubrahman1 <mahbuburrahman3@yahoo.com>
2022-12-19 23:01:06 +0600

    added booking form

--- src/assets/data/places.js
+++ src/assets/data/places.js
@@ -3,6 +3,7 @@ const places = [
         id: "coxsbazar",
         title: "Cox's Bazar",
         image: "https://i.ibb.co/68CQFrs/coxs-bazar.jpg",
+        description: "Cox's Bazar Beach located at Cox's Bazar, Bangladesh, is the longest natural sea beach in the world running 120 kilometres and 5th longest beach after Praia do Cassino of Brazil, Padre Island on the US Gulf Coast, Eighty Mile Beach in Western Australia, and Ninety Mile Beach of Australia. It is the top tourist destination of Bangladesh."
     },
     {
         id: "sajek",
